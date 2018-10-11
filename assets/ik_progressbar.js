5021df72f17afac9304b3c2c18b441b185afd982
Mike Shanafelt <mes6074@psu.edu>
2018-10-11 15:02:57 -0400

    Activity 4

--- assets/ik_progressbar.js
+++ assets/ik_progressbar.js
@@ -2,7 +2,8 @@
 	
 	var pluginName = 'ik_progressbar',
 		defaults = { // values can be overitten by passing configuration options to plugin constructor 
-			'max': 100
+			'max': 100,
+			'instructions': 'Press spacebar, or Enter to get progress',
 		};
 	
 	/**
@@ -31,17 +32,36 @@
 		this.element
 			.attr({
 				'id': id,
+				'tabindex': -1, // add current element to tab oder
+				'role': 'progressbar', // assign  progressbar role
+				'aria-valuenow': 0, // set current value to 0
+				'aria-valuemin': 0, // set minimum (start) value to 0 (required by screen readers)
+				'aria-valuemax': this.options.max, // set maximum (end) value
+				'aria-describedby': id + '_instructions' // add aria-describedby attribute 
 			})
 			.addClass('ik_progressbar')
-      ;
+			.on('keydown.ik', {'plugin': this}, this.onKeyDown);
 		
 		this.fill = $('<div/>')
 			.addClass('ik_fill');
 			
 		this.notification = $('<div/>') // add div element to be used to notify about the status of download
+			.attr({
+				'aria-live': 'assertive', // set notofocation priority to high
+				'aria-atomic': 'additions' // notify only about newly added text
+			})
 			.addClass('ik_readersonly')
 			.appendTo(this.element);
-
+			
+		$('<div/>') // add div element to be used with aria-described attribute of the progressbar
+			.text(this.options.instructions) // get instruction text from plugin options
+				.addClass('ik_readersonly') // hide element from visual display
+				.attr({
+					'id': id + '_instructions',
+					'aria-hidden': 'true'  // hide element from screen readers to prevent it from being read twice
+				})
+		.appendTo(this.element);
+		
 		$('<div/>')
 			.addClass('ik_track')
 			.append(this.fill)
@@ -58,7 +78,7 @@
 		
 		var value;
 		
-		value = Number( this.element.data('value') ); // inaccessible
+		value = Number( this.element.attr('aria-valuenow') ); // accessible
 		
 		return parseInt( value );
 		
@@ -99,9 +119,12 @@
 		}
 		
 		this.element
-			.data({ // inaccessible
+			/*.data({ // inaccessible
 				'value': parseInt(val) 
-			}) 
+			})*/
+			.attr({ // accessible
+				'aria-valuenow': val
+			});			
       ;
 		
 		this.updateDisplay();
@@ -131,6 +154,21 @@
 		this.updateDisplay();
 		this.notify();
 	
+	};
+	
+	Plugin.prototype.onKeyDown = function(event) {
+       
+		switch(event.keyCode) {
+			   
+			case ik_utils.keys.space:
+			case ik_utils.keys.enter:
+				event.preventDefault();
+				event.stopPropagation();
+				event.data.plugin.notify();
+				break;
+		}
+	 
+		   
 	};
 	
 	$.fn[pluginName] = function ( options ) {
